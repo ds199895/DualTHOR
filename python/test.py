f1e79ff649a4846b95f89e2929cc67dab92235db
ds199895 <2410831777@qq.com>
2025-03-06 23:41:20 +0800

    pushfortest

--- python/test.py
+++ python/test.py
@@ -9,19 +9,10 @@ def test_controller():
     # 启动控制器
     controller.start()
     
-    # # 测试加载机器人
-    # logging.info("Testing load_robot...")
-    # controller.load_robot('X1')
-    
-    # 测试重置场景
-    # logging.info("Testing reset_scene...")
-    # # controller.reset_scene('livingroom2', 'h1')
-
-    # controller.step('resetscene',scene="living_room2",robottype="h1")
 
     cur_state=controller.step("getcurstate")
 
-    # print("cur state: ",cur_state)
+    print("cur state: ",cur_state)
 
     # 测试move
     logging.info("Testing move...")
@@ -31,30 +22,9 @@ def test_controller():
 
     # feedback=controller.step("moveright",magnitude=1)
 
-
-    
     
 
-    # logging.info("Testing undo")
-
-    # if(not feedback['success']):
-    #     feedback=controller.step("undo")
-    
-
-    # print("feedback res",feedback);
-
-
-
-    # # 测试撤销动作
-    # logging.info("Testing undo_last_action...")
-    # controller.undo_last_action()
-    
-    # # 测试重做动作
-    # logging.info("Testing redo_last_action...")
-    # controller.redo_last_action()
-
-    # 等待特定信号
-
+    # 保持运行
     while True:
         time.sleep(1)
 
